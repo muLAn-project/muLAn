07ee77d7c0e5cbec64be1a26f680029fd0119c08
ArnaudCassan <cassan@iap.fr>
2017-09-16 19:37:15 +0200

    Update mulan.py

--- muLAn/template/mulan.py
+++ muLAn/template/mulan.py
@@ -5,23 +5,7 @@
 # Copyright (c) 2014-2017 The muLAn project team
 # Copyright (c) 2014-2017 Clément Ranc & Arnaud Cassan
 # 
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+# See the LICENCE file
 # =============================================================================
 
 # --------------------------------------------------------------------
@@ -213,7 +197,7 @@ if (__name__ == "__main__"):
     module_path = '{:s}packages'.format(path_mulan)
     add_topythonpath(module_path)
     # Check packages
-    check_packages(path_mulan, verbose=verbose)
+    ##removed## check_packages(path_mulan, verbose=verbose)
 
     # Load standard packages (after adding path to local packages)
     argparse = importlib.import_module('argparse')
