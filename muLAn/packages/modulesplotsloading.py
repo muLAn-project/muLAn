250ecc24af65f404bd578c545031ae3d7ab506eb
Clément Ranc <clement.ranc@gmail.com>
2017-10-13 23:54:36 -0400

    No need external file to load plot routines anymore: delete the file.

--- muLAn/packages/modulesplotsloading.py
+++
@@ -1,7 +0,0 @@
-# -*-coding:Utf-8 -*
-import sys
-sys.path.insert(0, '/Users/Herbie/Recherche/Projets/Miiriads/Softwares/muLAn-project/muLAn/muLAn/plottypes')
-import fitflux as fitflux
-def load():
-	modules_files = [fitflux]
-	return modules_files
