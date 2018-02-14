2314e6879ebaceb47705374949f9f1fb09c61de1
Clément Ranc <clement.ranc@gmail.com>
2018-02-14 16:51:17 -0500

    Add source position (x, y) to the outputs files. Change the column order.

--- muLAn/plottypes/fitmag.py
+++ muLAn/plottypes/fitmag.py
@@ -1271,8 +1271,8 @@ def plot(cfgsetup=False, models=False, model_param=False, time_serie=False, \
                 flag_fom = obs_properties['fluxoumag'][idx]
 
                 if flag_fom.lower()=='magnitude':
-                    text = "#{11:>5s} {0:>18s} {1:>6s} {3:>12s} {4:>10s} {8:>8s} {9:>9s} {10:>9s} {5:>12s} {6:>12s} {7:>9s} {12:>20s} {2:>20s}\n".format(
-                            "Date", "Magn", "Err_Magn", "Err_Magn_Res", "Resi", "Back", "Seeing", "Chi2", "Mgf-dat", "Err_Mgf", "Resi-Mgf", "ID", "Input_Magn")
+                    text = "#{11:>5s} {0:>18s} {1:>6s} {3:>12s} {4:>10s} {8:>8s} {9:>9s} {10:>9s} {5:>12s} {6:>12s} {13:>10s} {14:>10s} {7:>9s} {12:>20s} {2:>20s}\n".format(
+                            "Date", "Magn", "Err_Magn", "Err_Magn_Res", "Resi", "Back", "Seeing", "Chi2", "Mgf-dat", "Err_Mgf", "Resi-Mgf", "ID", "Input_Magn", "x", "y")
 
                 elif flag_fom.lower()=='flux':
                     text = "#{11:>5s} {0:>18s} {1:>6s} {3:>12s} {4:>10s} {8:>8s} {9:>9s} {10:>9s} {5:>12s} {6:>12s} {7:>9s} {12:>20s} {2:>20s}\n".format(
@@ -1287,7 +1287,7 @@ def plot(cfgsetup=False, models=False, model_param=False, time_serie=False, \
                 if flag_fom.lower()=='magnitude':
                     for jj in xrange(len(time_serie_SC['dates'])):
                         text = text +\
-                                "{11:6d} {0:18.12f} {1:6.3f} {3:12.3e} {4:10.3e} {8:8.3f} {9:9.3e} {10:9.2e} {5:12.5f} {6:12.5f} {7:9.3e} {12:20.12f} {2:20.12f}".format(
+                                "{11:6d} {0:18.12f} {1:6.3f} {3:12.3e} {4:10.3e} {8:8.3f} {9:9.3e} {10:9.2e} {5:12.5f} {6:12.5f} {13:10.6f} {14:10.6f} {7:9.3e} {12:20.12f} {2:20.12f}".format(
                                 time_serie_SC['dates'][jj],
                                 time_serie_SC['mag_align'][jj],
                                 time_serie_SC['err_magn_orig'][jj],
@@ -1300,14 +1300,16 @@ def plot(cfgsetup=False, models=False, model_param=False, time_serie=False, \
                                 time_serie_SC['mgf_data_err'][jj],
                                 time_serie_SC['res_mgf'][jj],
                                 time_serie_SC['id'][jj],
-                                time_serie_SC['magnitude'][jj]
+                                time_serie_SC['magnitude'][jj],
+                                time_serie_SC['x'][jj],
+                                time_serie_SC['y'][jj]
                                 )
                         text = text + "\n"
 
                 elif flag_fom.lower()=='flux':
                     for jj in xrange(len(time_serie_SC['dates'])):
                         text = text +\
-                                "{11:6d} {0:18.12f} {1:6.3f} {3:12.3e} {4:10.3e} {8:8.3f} {9:9.3e} {10:9.2e} {5:12.5f} {6:12.5f} {7:9.3e} {12:20.12f} {2:20.12f}".format(
+                                "{11:6d} {0:18.12f} {1:6.3f} {3:12.3e} {4:10.3e} {8:8.3f} {9:9.3e} {10:9.2e} {5:12.5f} {6:12.5f} {13:10.6f} {14:10.6f} {7:9.3e} {12:20.12f} {2:20.12f}".format(
                                 time_serie_SC['dates'][jj],
                                 time_serie_SC['mag_align'][jj],
                                 time_serie_SC['err_flux_orig'][jj],
@@ -1320,7 +1322,9 @@ def plot(cfgsetup=False, models=False, model_param=False, time_serie=False, \
                                 time_serie_SC['mgf_data_err'][jj],
                                 time_serie_SC['res_mgf'][jj],
                                 time_serie_SC['id'][jj],
-                                time_serie_SC['flux'][jj]
+                                time_serie_SC['flux'][jj],
+                                time_serie_SC['x'][jj],
+                                time_serie_SC['y'][jj]
                                 )
                         text = text + "\n"
 
