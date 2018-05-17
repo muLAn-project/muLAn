f644d2f6364c8d8f63c2f4e97543572fc2dcfcca
Clément Ranc <clement.ranc@gmail.com>
2018-05-16 22:54:13 -0400

    Add tests in fsfbwsig functions to avoid NaN and remove inf chi2

--- muLAn/models/grid_dmcmc.py
+++ muLAn/models/grid_dmcmc.py
@@ -465,9 +465,9 @@ def lnprob(theta, time_serie, model_params, fitted_param, nuisance, models_names
             chi2 = np.sum(time_serie['chi2pp'])
             result = - chi2/2.0 + lnprior_curr
         else:
-            result = lnprior_curr
+            result = 1e12
     else:
-        result = lnprior_curr
+        result = 1e12
 
     return result
 
@@ -490,25 +490,48 @@ def fsfb(time_serie, cond, blending=True):
 
 # ----------------------------------------------------------------------
 def fsfbwsig(time_serie, cond, blending=True):
+    """
+    Compute the source and blend flux using a linear fit with errors
+    in flux.
+
+    Parameters
+    ----------
+    time_serie : dict
+        A dictionnary with the data flux (`flux`) and errors
+        ('err_flux'), the corresponding magnification from the model
+        (`amp`).
+
+    cond : array-like
+        Array of booleans used as a mask for ``time_serie``.
+
+    blending : bool, default True
+        Wheter or not fitting with a blending.
+
+    Returns
+    -------
+    fs : float
+        A float which is the source flux.
+    fb : float
+        A float which is the blend flux.
+    """
 
     x = np.atleast_2d(time_serie['amp'][cond]).T
     y = np.atleast_2d(time_serie['flux'][cond]).T
     sig = np.atleast_2d(time_serie['err_flux'][cond]).T
-
     x2 = np.power(x, 2)
     sig2 = np.power(sig, 2)
-    s = np.sum(1.0 / sig2)
-    sx = np.sum(x / sig2)
-    sy = np.sum(y / sig2)
-    sxx = np.sum(x2 / sig2)
-    sxy = np.sum(x * y / sig2)
-    den = s * sxx - sx**2
 
     if blending:
-        if den > 1e-20:
+        try:
+            s = np.sum(1.0 / sig2)
+            sx = np.sum(x / sig2)
+            sy = np.sum(y / sig2)
+            sxx = np.sum(x2 / sig2)
+            sxy = np.sum(x * y / sig2)
+            den = s * sxx - sx**2
             fs = (s * sxy - sx * sy) / den
             fb = (sxx * sy - sx * sxy) / den
-        else:
+        except ZeroDivisionError:
             fs = np.inf
             fb = np.inf
     else:
