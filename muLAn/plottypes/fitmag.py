778910d884d23313ec195453f991a98aa86128c5
Clément Ranc <clement.ranc@gmail.com>
2018-05-24 17:32:08 -0400

    Create linear fit with error bars.

--- muLAn/plottypes/fitmag.py
+++ muLAn/plottypes/fitmag.py
@@ -158,25 +158,52 @@ def fsfb(time_serie, cond, blending=True):
 
 # ----------------------------------------------------------------------
 def fsfbwsig(time_serie, cond, blending=True):
+    """
+    Compute the source and blend flux using a linear fit with errors
+    in flux.
+
+    Parameters
+    ----------
+    time_serie : dict
+        A dictionnary with the data flux (`flux`) and errors
+        ('err_flux'), the corresponding magnification from the model
+        (`amp`).
+
+    cond : array-like
+        Array of booleans used as a mask for ``time_serie``.
+
+    blending : bool, default True
+        Wheter or not fitting with a blending.
+
+    Returns
+    -------
+    fs : float
+        A float which is the source flux.
+    fb : float
+        A float which is the blend flux.
+    """
 
     x = np.atleast_2d(time_serie['amp'][cond]).T
     y = np.atleast_2d(time_serie['flux'][cond]).T
     sig = np.atleast_2d(time_serie['err_flux'][cond]).T
-
     x2 = np.power(x, 2)
     sig2 = np.power(sig, 2)
-    s = np.sum(1.0 / sig2)
-    sx = np.sum(x / sig2)
-    sy = np.sum(y / sig2)
-    sxx = np.sum(x2 / sig2)
-    sxy = np.sum(x * y / sig2)
-    den = s * sxx - sx**2
 
     if blending:
-        fs = (s * sxy - sx * sy) / den
-        fb = (sxx * sy - sx * sxy) / den
+        try:
+            s = np.sum(1.0 / sig2)
+            sx = np.sum(x / sig2)
+            sy = np.sum(y / sig2)
+            sxx = np.sum(x2 / sig2)
+            sxy = np.sum(x * y / sig2)
+            den = s * sxx - sx**2
+            fs = (s * sxy - sx * sy) / den
+            fb = (sxx * sy - sx * sxy) / den
+        except ZeroDivisionError:
+            fs = np.inf
+            fb = np.inf
     else:
-        fb = 0.0
+        fs, fb = fsfb(time_serie, cond, blending=False)
 
     return fs, fb
 
