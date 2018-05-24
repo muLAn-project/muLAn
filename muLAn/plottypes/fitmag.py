fe9fe51d00479c2aeacfd7abdd4957f5b3152049
Clément Ranc <clement.ranc@gmail.com>
2018-05-24 18:17:54 -0400

    Moove fsfb and fsfbwsig to packages/algebra

--- muLAn/plottypes/fitmag.py
+++ muLAn/plottypes/fitmag.py
@@ -66,6 +66,7 @@ from matplotlib.ticker import FixedLocator, FormatStrFormatter
 #  Non-standard packages
 # ----------------------------------------------------------------------
 import muLAn.models.ephemeris as ephemeris
+import muLAn.packages.algebra as algebra
 
 
 # import models.esblparall as esblparall
@@ -135,77 +136,6 @@ def unpack_options(cfgsetup, level0, level1, sep=','):
     options = [a.strip() for a in cfgsetup.get(level0, level1).split(sep)]
     del a, cfgsetup, level0, level1
     return options
-
-
-# ----------------------------------------------------------------------
-def fsfb(time_serie, cond, blending=True):
-
-    #blending = True
-
-    x = np.atleast_2d(time_serie['amp'][cond]).T
-    y = np.atleast_2d(time_serie['flux'][cond]).T
-
-    regr = linear_model.LinearRegression(fit_intercept=blending)
-    regr.fit(x, y)
-    fs = regr.coef_[0][0]
-    # fb = regr.intercept_[0]
-    if blending:
-        fb = regr.intercept_[0]
-    else:
-        fb = 0.0
-
-    return fs, fb
-
-# ----------------------------------------------------------------------
-def fsfbwsig(time_serie, cond, blending=True):
-    """
-    Compute the source and blend flux using a linear fit with errors
-    in flux.
-
-    Parameters
-    ----------
-    time_serie : dict
-        A dictionnary with the data flux (`flux`) and errors
-        ('err_flux'), the corresponding magnification from the model
-        (`amp`).
-
-    cond : array-like
-        Array of booleans used as a mask for ``time_serie``.
-
-    blending : bool, default True
-        Wheter or not fitting with a blending.
-
-    Returns
-    -------
-    fs : float
-        A float which is the source flux.
-    fb : float
-        A float which is the blend flux.
-    """
-
-    x = np.atleast_2d(time_serie['amp'][cond]).T
-    y = np.atleast_2d(time_serie['flux'][cond]).T
-    sig = np.atleast_2d(time_serie['err_flux'][cond]).T
-    x2 = np.power(x, 2)
-    sig2 = np.power(sig, 2)
-
-    if blending:
-        try:
-            s = np.sum(1.0 / sig2)
-            sx = np.sum(x / sig2)
-            sy = np.sum(y / sig2)
-            sxx = np.sum(x2 / sig2)
-            sxy = np.sum(x * y / sig2)
-            den = s * sxx - sx**2
-            fs = (s * sxy - sx * sy) / den
-            fb = (sxx * sy - sx * sxy) / den
-        except ZeroDivisionError:
-            fs = np.inf
-            fb = np.inf
-    else:
-        fs, fb = fsfb(time_serie, cond, blending=False)
-
-    return fs, fb
 
 # ----------------------------------------------------------------------
 def critic_roots(s, q, phi):
@@ -450,6 +380,9 @@ def boussole(EarthSunFile=False, EarthSatelliteFile=False, cfg=False, \
 # ----------------------------------------------------------------------
 def plot(cfgsetup=False, models=False, model_param=False, time_serie=False, \
          obs_properties=False, options=False, interpol_method=False):
+
+
+    algebra.test()
 
     #    Initialisation of parameters
     # ------------------------------------------------------------------
@@ -865,7 +798,7 @@ def plot(cfgsetup=False, models=False, model_param=False, time_serie=False, \
 
                 # Calculation of fs and fb
                 # fs, fb = fsfb(time_serie, cond2, blending=True)
-                fs, fb = fsfbwsig(time_serie, cond2, blending=True)
+                fs, fb = algebra.fsfbwsig(time_serie, cond2, blending=True)
                 # if (fb/fs < 0 and observatories[j]=="ogle-i"):
                 #     fs, fb = fsfb(time_serie, cond2, blending=False)
 
